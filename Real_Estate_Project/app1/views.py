86866671c684f85e0394afa1db96a340f12044bd
blenfasil <blenfasil7@gmail.com>
2024-02-29 11:58:20 -0800

    Add new feature

--- Real_Estate_Project/app1/views.py
+++ Real_Estate_Project/app1/views.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render
+from django.shortcuts import redirect, HttpResponse
 from .models import Properties, Registration
 
 from django.core.mail import send_mail
 from Real_Estate_Project import settings
 from .models import Registration
+from django.contrib.auth.models import User  
+from django.contrib import messages
 
 
 def home_view(request):
     return render(request, 'home.html')
 
 def registration_view(request):
+    if request.method == 'POST':
+        firstName = request.POST['firstName']
+        lastName = request.POST['lastName']
+        email = request.POST['email']
+        username = request.POST['username']
+        address = request.POST['address']
+        password = request.POST['password']
+        confirmPassword = request.POST['confirmPassword']
+        
+        myuser = User.objects.create_user(username, email, password)
+        myuser.first_name = firstName
+        myuser.last_name = lastName
+        myuser.save()
+        messages.success(request, "Registration successful")
+
+        return redirect('home')
+
+    # Handle GET request or other request methods here
+    # For example, you can render a registration form
     return render(request, 'registration.html')
 
 def property_listing(request):
